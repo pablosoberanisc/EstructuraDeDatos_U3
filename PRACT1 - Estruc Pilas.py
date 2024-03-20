935df5f96b2fc1905f0b1f0762c2fef666d551ae
Pablo David Soberanis Cetz <145534224+pablosoberanisc@users.noreply.github.com>
2024-03-19 20:16:37 -0600

    Add files via upload

---
+++ PRACT1 - Estruc Pilas.py
@@ -0,0 +1,44 @@
+class Pila:
+    def __init__(self, tamanio_max):
+        self.tope = 0
+        self.pila = [None] * tamanio_max
+
+    def esta_vacia(self):
+        return self.tope == 0
+
+    def esta_llena(self):
+        return self.tope == len(self.pila)
+
+    def insertar(self, dato):
+        if self.esta_llena():
+            print("Error: Desbordamiento de la pila")
+        else:
+            self.pila[self.tope] = dato
+            self.tope += 1
+
+    def eliminar(self):
+        if self.esta_vacia():
+            print("Error: Subdesbordamiento de la pila")
+        else:
+            self.tope -= 1
+            return self.pila[self.tope]
+
+
+pila = Pila(8)
+
+pila.insertar("X")
+pila.insertar("Y")
+
+pila.eliminar()
+pila.eliminar()
+pila.eliminar()
+
+pila.insertar("V")
+pila.insertar("W")
+
+pila.eliminar()
+
+pila.insertar("R")
+
+
+print("Elementos en la pila:", pila.pila[:pila.tope])
