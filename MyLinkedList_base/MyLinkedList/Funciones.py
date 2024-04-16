d938b01aa679b3f7304ca0a434d83d28695e0fbd
Pablo David Soberanis Cetz <145534224+pablosoberanisc@users.noreply.github.com>
2024-04-15 23:09:08 -0600

    Update Funciones.py

--- MyLinkedList_base/MyLinkedList/Funciones.py
+++ MyLinkedList_base/MyLinkedList/Funciones.py
@@ -1,11 +1,50 @@
-import fitz 
+def create_linked_list():
+  class node:
+      def __init__(self, data=None, next=None):
+          self.data = data
+          self.next = next
 
-def leer_pdf(path):
-    try:
-        with fitz.open(path) as file:
-            text = ''
-            for page in file:
-                text += page.getText()
-        print(text)
-    except:
-        pass
+  class linked_list:
+      def __init__(self):
+          self.head = None
+
+      def add_at_front(self, data):
+          self.head = node(data=data, next=self.head)
+
+      def is_empty(self):
+          return self.head == None
+
+      def add_at_end(self, data):
+          if not self.head:
+              self.head = node(data=data)
+              return
+          curr = self.head
+          while curr.next:
+              curr = curr.next
+          curr.next = node(data=data)
+
+      def delete_node(self, key):
+          curr = self.head
+          prev = None
+          while curr and curr.data != key:
+              prev = curr
+              curr = curr.next
+          if prev is None:
+              self.head = curr.next
+          elif curr:
+              prev.next = curr.next
+              curr.next = None
+
+      def get_last_node(self):
+          temp = self.head
+          while temp.next is not None:
+              temp = temp.next
+          return temp.data
+
+      def print_list(self):
+          node = self.head
+          while node != None:
+              print(node.data, end=" => ")
+              node = node.next
+
+  return linked_list() 
